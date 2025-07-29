637d9d96680832c898d9eb6f4b64887f3df05ed4
Francis20922 <francisonwuka2092@gmail.com>
2025-07-29 11:46:05 +0100

    second commit

--- quickpay/lib/main.dart
+++ quickpay/lib/main.dart
@@ -7,7 +7,7 @@ import 'theme_notifier.dart'; // <-- import the theme notifier
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform, 
   );
 
   runApp(const QuickPayApp());
